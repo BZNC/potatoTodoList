cf302a6efb02d5101e63daca933f7eb0e2a91c4b
BZNC <75849735+BZNC@users.noreply.github.com>
2021-08-03 16:15:08 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -50,19 +50,24 @@ $(function () {
     }
 
     // 4 添加todo的方法
-    $('#title').on("keydown", function (event) {
+ $("#title").on("keydown", function(event) {
         if (event.keyCode === 13) {
-            console.log($(this))
-            if ($(this).val() !== "") {
-                let storaged = readData()
-                storaged.push({ title: $(this).val(), done: false })
-                writeData(storaged)
-                console.log(readData())
-                load()
+            if ($(this).val() === "") {
+                alert("请输入您要的操作");
+            } else {
+                // 先读取本地存储原来的数据
+                var local = getDate();
+                // console.log(local);
+                // 把local数组进行更新数据 把最新的数据追加给local数组
+                local.push({ title: $(this).val(), done: false });
+                // 把这个数组local 存储给本地存储
+                saveDate(local);
+                // 2. toDoList 本地存储数据渲染加载到页面
+                load();
+                $(this).val("");
             }
         }
-
-    })
+    });
 
     //5 删除list的方法
     $("ul,ol").on("click", "a", function () {
